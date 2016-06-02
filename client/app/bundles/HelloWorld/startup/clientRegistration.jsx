2e7b104be6c73ec4be6b5ccd6a7edc4b3dfa5899
Eric Firth <eric.firth@persado.com>
2016-06-02 13:48:18 -0400

    initial commit

---
+++ client/app/bundles/HelloWorld/startup/clientRegistration.jsx
@@ -0,0 +1,5 @@
+import ReactOnRails from 'react-on-rails';
+import ScoreBoardApp from './scoreboardAppClient';
+
+// This is how react_on_rails can see the HelloWorldApp in the browser.
+ReactOnRails.register({ ScoreBoardApp });
---
+++ client/app/bundles/HelloWorld/startup/scoreboardAppClient.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Scoreboard from '../components/scoreboard';
+
+export default (props) => (
+  <Scoreboard {...props} />
+);

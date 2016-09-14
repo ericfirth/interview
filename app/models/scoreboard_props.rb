5181fe781a82b91c7780376d68f400d048fec393
Eric Firth <eric.firth@persado.com>
2016-09-14 16:49:58 -0400

    Phase Two started

--- app/models/scoreboard_props.rb
+++ app/models/scoreboard_props.rb
@@ -11,7 +11,7 @@ class ScoreboardProps
   
   def phase_one
     props = empty_prop_object
-    props[:instructions] = "You are a new employee of a brand new website for gamers. Your goal is to make a scoreboard table. Take the props, and this table skeleton and build us scoreboard."
+    props[:instructions] = "You are a new employee of a brand new website for gamers. Your goal is to make a scoreboard table. Take the props, and this table skeleton and build us scoreboard. BTW we offshored the API that you will receive them from to a developer off-shore. Good Luck!"
     props[:extra_tasks] << "sort the scores so they go high - low"
     props[:extra_tasks] << "make the player names a link that filter the list to show only that players scores"
     props[:extra_tasks] << "make a link to show all the scores and have a header above the table that shows what the table is"
@@ -21,11 +21,18 @@ class ScoreboardProps
   end
   
   def phase_two
-    empty_prop_object
+    props = empty_prop_object    
+    props[:instructions] = "Your company is taking off and now there are lots and lots of scoreboards, each for its own game company, that need to go everywhere. More problems with the API, though. You have no idea now what the scores object will look like, as each game company wants to highlight different things. You need to abstract that table! You did get the backend developers to agree to give you an array of what the elements are in order for any given table, though."
+    props[:extra_tasks] << "none yet"
+    props[:component_props] = phase_two_component_props
+    
+    props
   end
   
   def phase_three
-    empty_prop_object
+    props = empty_prop_object
+    
+    props
   end
   
   private
@@ -44,6 +51,21 @@ class ScoreboardProps
     end
     
     scores
+  end
+  
+  def phase_two_component_props
+    props = {}
+    props[:columsn] = ["player_name", "game", "headshots", "power_ups_collected", "tournament", "score"]
+    props[:gm_company] = "Funimated Games, Inc."
+    scores = []
+    
+    50.times do 
+      scores << { falcon_hunter: [true, false].sample, player_name: PLAYERS.sample, headshots: rand(500...20_000), score: rand(10_000...1_000_000), power_ups_collected: rand(400...1000), tournament: [true, false].sample }
+    end
+    
+    props[:scores] = scores
+    
+    props
   end
   
 end
--- client/app/bundles/HelloWorld/components/phase_two.jsx
+++ client/app/bundles/HelloWorld/components/phase_two.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 
 export default class PhaseTwo extends React.Component {
   constructor(props, context) {
@@ -7,7 +7,30 @@ export default class PhaseTwo extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="scoreboard container">
+        <h1>HEADER</h1>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Player</th>
+              <th>Game</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.props.scores.map(function (score, index) {
+                return (
+                  <tr key={index}>
+                    <td>{score.player}</td>
+                    <td>{score.gamez}</td>
+                    <td>{score.score}</td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
       </div>
     );
   }
